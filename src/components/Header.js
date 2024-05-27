0c2ec16b23ad8aa44377ec7894eb7c9dc9b8dc93
ozyonumb <ozyonumb@getalp-athena6.imag.fr>
2024-05-27 13:44:00 +0200

    traduction form OK

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,21 +28,21 @@ export const Header = ({word, setWord, tralang, setTralang}) => {
     <div className="bg-gray-700">
       <div className='container mx-auto py-8'>
         <h1 className='text-3xl font-bold test-center text-white'>Dictionnaire Simple</h1>
-        <p className='text-center mt-1 mb-10 text-slate-300 text-lg'>Trouvez la définition du mot {word}</p>
+        <p className='text-center mt-1 mb-10 text-slate-300 text-lg'>Trouvez la définition et la traduction du mot {word}</p>
         <div className='flex item-center justify-center mt-5'>
-          <div className='flex border-2 border-gray-200'>
-            <input type='text' className='px-4 py-2 w-80' placeholder='Search...' onChange={handleInputChange}/>
-            <button className='bg-blue-400 text-white px-4'
-            onClick={handleSubmit}>Rechercher</button>
-            <form className='bg-blue-500'>
-              <label className='text-black px-4 pos-relative'>
-                <select className='display-none'onChange={handleSelectChange}>
+          <div className='flex border-3 border-gray-200'>
+            <input type='text' className='px-4 py-2 w-80' placeholder='Search for a word in english...' onChange={handleInputChange}/>
+            <form>
+              <label>
+                <select className='flex bg-blue-500 text-white py-3 px-4 appearence-none'onChange={handleSelectChange}>
                   <option value="fr">Français</option>
-                  <option value="en">Anglais</option>
                   <option value="tr">Turc</option>
                 </select>
               </label>
             </form>
+            <button className='bg-blue-400 text-white px-4'
+            onClick={handleSubmit}>Rechercher</button>
+            
           </div>
         </div>
       </div> 
